3cc22748e37781d06533208ca6ec4cfd50c2862e
frangian <franciscogianzanti@gmail.com>
2023-05-29 17:27:34 -0300

    mensajes de error en login y signup

--- scripts/login.js
+++ scripts/login.js
@@ -14,19 +14,27 @@ window.addEventListener("load", function () {
   const form = document.querySelector("form");
   const url = "http://localhost:8080/api/v1/usuario";
 
+  var inputPassword = document.getElementById("inputPassword");
+
   /* -------------------------------------------------------------------------- */
   /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
   /* -------------------------------------------------------------------------- */
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
+    eliminarMensajeError();
+
     const formData = {};
-    formData.email = inputEmail.value;
-    formData.password = inputContrasena.value;
+    if (validarEmail(inputEmail)) {
+      formData.email = inputEmail.value;
+      formData.password = inputContrasena.value;
+      //   console.log(formData);
+      realizarLogin(formData);
+    } else {
+      console.log("datos ingresados incorrectamente");
 
-    console.log(formData);
-
-    realizarLogin(formData);
+      mostrarMensajeError("Datos ingresados incorrectamente")
+    }
   });
 
   /* -------------------------------------------------------------------------- */
@@ -40,25 +48,47 @@ window.addEventListener("load", function () {
       },
       body: JSON.stringify(formData),
     };
-
+  
     fetch(url + "/auth/login", settings)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status === 403) {
+          throw new Error("Acceso denegado. Credenciales ingresadas incorrectas.");
+        }
+        return response.json();
+      })
       .then((data) => {
         console.log("Respuesta del servidor:", data);
         // Hacer algo con la respuesta del servidor
-
+  
         // Guardar los datos en el localStorage
         localStorage.setItem("id", data.id);
         localStorage.setItem("nombre", data.nombre);
         localStorage.setItem("apellido", data.apellido);
         localStorage.setItem("email", data.email);
-
+  
         // Redirigir a la página mis-tareas.html
         window.location.href = "mis-tareas.html";
       })
       .catch((error) => {
         console.error("Error:", error);
-        // Manejar el error en caso de que ocurra
+        mostrarMensajeError("Verifica tus credenciales")
       });
+  }
+
+  function mostrarMensajeError(mensaje) {
+    var div = document.createElement("div");
+    div.id = "mensaje-error";
+    div.innerHTML = mensaje;
+    div.style.color = "red";
+    div.style.padding = "15px";
+    div.style.textAlign = "center";
+    inputPassword.insertAdjacentElement("afterend", div);
+  }
+
+  function eliminarMensajeError() {
+    var mensajeError = document.getElementById("mensaje-error");
+    if (mensajeError) {
+      mensajeError.remove();
+    }
   }
 });
--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,98 +1,76 @@
 // SEGURIDAD: Si no se encuentra en localStorage info del usuario
 // no lo deja acceder a la página, redirigiendo al login inmediatamente.
-
-
-
+if (!localStorage.email) {
+  location.replace('./index.html');
+}
 /* ------ comienzan las funcionalidades una vez que carga el documento ------ */
-window.addEventListener('load', function () {
-
+window.addEventListener("load", function () {
   /* ---------------- variables globales y llamado a funciones ---------------- */
-  
-
-
+  const btnCerrarSesion = document.querySelector("#closeApp");
   /* -------------------------------------------------------------------------- */
   /*                          FUNCIÓN 1 - Cerrar sesión                         */
   /* -------------------------------------------------------------------------- */
 
-  btnCerrarSesion.addEventListener('click', function () {
-   
-
-
-
+  btnCerrarSesion.addEventListener("click", function () {
+    localStorage.clear();
+    location.replace("./index.html");
   });
+
+  obtenerNombreUsuario();
 
   /* -------------------------------------------------------------------------- */
   /*                 FUNCIÓN 2 - Obtener nombre de usuario [GET]                */
   /* -------------------------------------------------------------------------- */
 
   function obtenerNombreUsuario() {
-   
+    // const settings = {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // };
 
+    // fetch(urlUsuario, settings)
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     showUserName(data.nombre + " " + data.apellido);
+    //   });
 
+    showUserName();
 
-  };
-
+    function showUserName() {
+      const userText = document.querySelector(".user-info p");
+      userText.innerText =
+        localStorage.getItem('nombre') + " " + localStorage.getItem('apellido');
+    }
+  }
 
   /* -------------------------------------------------------------------------- */
   /*                 FUNCIÓN 3 - Obtener listado de tareas [GET]                */
   /* -------------------------------------------------------------------------- */
 
-  function consultarTareas() {
-    
-    
-
-
-
-  };
-
+  function consultarTareas() {}
 
   /* -------------------------------------------------------------------------- */
   /*                    FUNCIÓN 4 - Crear nueva tarea [POST]                    */
   /* -------------------------------------------------------------------------- */
 
-  formCrearTarea.addEventListener('submit', function (event) {
-    
-
-
-
-
-  });
-
+  formCrearTarea.addEventListener("submit", function (event) {});
 
   /* -------------------------------------------------------------------------- */
   /*                  FUNCIÓN 5 - Renderizar tareas en pantalla                 */
   /* -------------------------------------------------------------------------- */
-  function renderizarTareas(listado) {
-
-
-
-
-
-
-
-  };
+  function renderizarTareas(listado) {}
 
   /* -------------------------------------------------------------------------- */
   /*                  FUNCIÓN 6 - Cambiar estado de tarea [PUT]                 */
   /* -------------------------------------------------------------------------- */
-  function botonesCambioEstado() {
-    
-    
-
-
-
-  }
-
+  function botonesCambioEstado() {}
 
   /* -------------------------------------------------------------------------- */
   /*                     FUNCIÓN 7 - Eliminar tarea [DELETE]                    */
   /* -------------------------------------------------------------------------- */
-  function botonBorrarTarea() {
-   
-    
-
-    
-
-  };
-
+  function botonBorrarTarea() {}
 });
